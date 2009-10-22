05b758cedc660c3dcee61a86125ee92c23ad502c
Joel Lennartsson <joel@crunchbang.(none)>
2009-10-22 16:56:08 +0200

    added simulator stuff for real this time

---
+++ src/simulator.h
@@ -0,0 +1,23 @@
+#ifndef PROJECT_SIMULATOR_H
+#define PROJECT_SIMULATOR_H
+
+class Simulator
+{
+    public:      
+
+        Simulator();
+        ~Simulator();
+
+        void tick();
+        void render();
+        
+    protected:
+    private:
+
+        
+};
+
+
+
+#endif //PROJECT_SIMULATOR_H
+
